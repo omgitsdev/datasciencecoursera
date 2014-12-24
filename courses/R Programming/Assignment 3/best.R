c4b8463d9289bb74b64dd1fe0d063fac73acf625
Dev Sethi <omgitsdev@gmail.com>
2014-12-24 00:58:44 -0600

    best complete, passes submission tests

--- courses/R Programming/Assignment 3/best.R
+++ courses/R Programming/Assignment 3/best.R
@@ -3,7 +3,20 @@ best <- function(state, outcome) {
   outcomes <- read.csv('outcome-of-care-measures.csv')
   ## Check that state and outcome are valid
   if (!is.element(tolower(state), tolower(outcomes$State))) stop("invalid state")
-  if (!is.element(tolower(outcome), c("heart attack", "heart failure", "pneumonia"))) stop("invalid outcome")
+  if (tolower(outcome) == "heart attack") {
+    outcomeCol <- 11
+    outcomes[,11] <- as.numeric(as.character(outcomes[,11]))
+  } else if (tolower(outcome) == "heart failure") {
+    outcomeCol <- 17
+    outcomes[,17] <- as.numeric(as.character(outcomes[,17]))
+  } else if (tolower(outcome) == "pneumonia") {
+    outcomeCol <- 23
+    outcomes[,23] <- as.numeric(as.character(outcomes[,23]))
+  } else {
+    stop('invalid outcome')
+  }
   ## Return hospital name in that state with lowest 30-day death
-
+  s <- outcomes[outcomes$State == toupper(state), ]
+  ordered <- s[order(s[[outcomeCol]], s$Hospital.Name), ]
+  return (as.character(ordered[[2]][1]))
 }
