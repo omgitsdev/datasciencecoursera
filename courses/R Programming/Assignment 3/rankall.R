183589a15cc1e8a52faab1328ba9c45ad94c07bc
Dev Sethi <omgitsdev@gmail.com>
2014-12-23 21:59:55 -0600

    additional function stub

--- courses/R Programming/Assignment 3/rankall.R
+++ courses/R Programming/Assignment 3/rankall.R
@@ -1,3 +1,10 @@
+best <- function(state, outcome) {
+        ## Read outcome data
+        ## Check that state and outcome are valid
+        ## Return hospital name in that state with lowest 30-day death
+        ## rate
+}
+
 rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
         ## Check that state and outcome are valid
